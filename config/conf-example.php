f3a32d14471c9a6ef07a5e0632cb1431900ec292
Arturo <cosasscosas@gmail.com>
2021-10-03 20:05:15 +0200

    Cambios menores de configuración

--- config/conf-example.php
+++ config/conf-example.php
@@ -3,8 +3,8 @@
 define("DB_HOST", "");
 define("DB_USER", "");
 define("DB_NAME", "");
-define("DB_PASS", '');
-define("DB_CHARSET", 'utf8');
+define("DB_CHARSET", "utf8");
+define("DB_PREFIX", "mymap_");
 /*Your custom directory instead of root here,after the slash */
 define('ROOT_PATH',$_SERVER['DOCUMENT_ROOT']."/");
 
