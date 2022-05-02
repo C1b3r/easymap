53b6afe8045004b8740a7fdcc2862c1dd7388f1a
Arturo <cosasscosas@gmail.com>
2022-05-02 12:36:28 +0200

    Ejemplo de enrutamiento para login

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,14 @@ use app\routing\Router;
 /* Controllers zone */
 use app\controllers\Index_Controller;
 use app\controllers\admincontrollers\Admin_Controller;
+use app\controllers\admincontrollers\Login_Controller;
 
 $router = $this->router;
 // $router = new Router(new Request(), new Response());
 // $app->router->get('/', [SiteController::class, 'home']);
 $router->get('/',[Index_Controller::class, 'index'])->name('inicio');
+$router->get('/login',[Login_Controller::class, 'index'])->name('login');
+$router->post('/login',[Login_Controller::class, 'index'])->name('submitLogin');
 $router->group(ADMIN_FOLDER,function() use($router){
     $router->get('/',[Admin_Controller::class, 'index'])->name('login');
     $router->get('prueba',[Index_Controller::class, 'index'])->name('prueba');
