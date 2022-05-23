94f76b291e3843f2dc424a679199f3960bb27cbc
Arturo <cosasscosas@gmail.com>
2022-05-23 19:35:54 +0200

    Agregregados cambios para prohibir el acceso a vendor y por curiosidad ver cuanto tarda en cargar la pagina completa

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 define('ROOT_PATH',dirname(__FILE__).'/');
+//$start_time = microtime(true);
 //needed for composers libraries
 require_once(ROOT_PATH.'vendor/autoload.php');
 
 use app\classes\Boot;
 //initialize the application
 new Boot();
+//echo 'This page was generated in'.(number_format(microtime(true) - $start_time, 2,'.','')).'seconds';
