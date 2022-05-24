e18d8b6d3aa44a65251da988a032ddbad8159eca
Arturo <cosasscosas@gmail.com>
2022-05-24 11:02:25 +0200

    Se ha cambiado las referencias a boostrap para poder acceder a las librerías dentro de vendor, para ahorrar espacio y que no permanezcan en resources o compilarlas. Se ha agregado por separado el modo noche para quitar el defer y que carge de manera instantánea para que no se note el pequeño delay que tenía en custom

--- app/views/header.php
+++ app/views/header.php
@@ -7,8 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="<?php echo $this->robots; ?>">
     <title><?php echo $this->title; ?></title>
-    <link href="<?php echo PUBLIC_WEB_PATH.'bootstrap/css/bootstrap.min.css';?>" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link href="<?php echo PUBLIC_WEB_PATH.'css/styles-ref.css';?>" rel="stylesheet">
     <link rel="icon" type="image/png" href="<?php echo PUBLIC_WEB_PATH;?>/images/icons/favicon-16x16.png" sizes="16x16">
     <link rel="icon" type="image/png" href="<?php echo PUBLIC_WEB_PATH;?>/images/icons/favicon-32x32.png" sizes="32x32">
 </head>
