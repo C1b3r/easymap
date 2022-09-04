9897de7d68aa3fe32155601715209cf33ee57924
Arturo <cosasscosas@gmail.com>
2022-09-04 17:40:35 +0200

    Movido el grueso de la representación de secciones en una vista a parte de cara a integrarlo de manera dinámica en las vistas que la necesiten

---
+++ app/views/admin/secciones/mainSections.php
@@ -0,0 +1,18 @@
+<?php defined('ROOT_PATH') or exit('Direct access forbidden'); ?>
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+   <!-- active  -->
+  <?php $first = reset($this->secciones);
+  foreach($this->secciones as $section => $nombreSeccion):?>
+    <li class="nav-item" role="presentation">
+      <a class="nav-link" onclick="cargarContenido('<?php echo $section; ?>')" <?php echo ($nombreSeccion == $first)? "primerTab ": '' ;?>id="<?php echo $section; ?>-tab" href="#<?php echo $section; ?>" type="button" role="tab" aria-controls="<?php echo $section; ?>" aria-selected="true"><?php echo $nombreSeccion; ?></a>
+    </li>
+   
+  <?php endforeach;?>
+</ul>
+<section class="col-md-12">
+    <div class="tab-content" id="myTabContent">
+      <div class="tab-pane fade" id="resultTab" role="tabpanel" aria-labelledby="b">..90090909.</div>
+    </div>
+</section>
+<script>//alert(window.location.hash.substring(1)); //substring para quitar el #</script>
+<?php $this->putJSorStyle('js/editseccionesmapas.js','js');?>
