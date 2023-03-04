c5fc79ab7ee4529e94c2b2ea3f9f8482af0d6464
Arturo <cosasscosas@gmail.com>
2023-03-04 18:42:56 +0100

    Estructura de como será cargar contenido de una pestaña con el método fetch. Creada las metas de url para recogerlas en js

--- app/views/header.php
+++ app/views/header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="<?php echo $this->robots; ?>">
+    <meta name="url" content="<?php echo COMPLETE_WEB_PATH; ?>">
     <title><?php echo $this->title; ?></title>
     <link href="<?php echo PUBLIC_WEB_PATH.'css/styles-ref.css';?>" rel="stylesheet">
     <link rel="icon" type="image/png" href="<?php echo PUBLIC_WEB_PATH;?>/images/icons/favicon-16x16.png" sizes="16x16">
--- resources/js/editseccionesmapas.js
+++ resources/js/editseccionesmapas.js
@@ -16,6 +16,55 @@ function cargarTab(tab = '') {
 
 }
 
-function cargarContenido(url) {
-    //ajax
+function cargarContenido(urltab) {
+    // Seleccionar la etiqueta meta por su nombre
+    const metaUrl = document.querySelector('meta[name="url"]');
+
+    // Obtener el valor de la meta etiqueta
+    const url = metaUrl.getAttribute('content');
+    // Realizamos la llamada a la API utilizando la función fetch
+    fetch(url + urltab)
+    .then(response => response.json()) // Convertimos la respuesta a JSON
+    .then(data => {
+    // Obtenemos los datos del JSON
+    const titulo = data.titulo;
+    const descripcion = data.descripcion;
+
+    // Creamos un nuevo elemento de título
+    const tituloElemento = document.createElement('h2');
+    tituloElemento.textContent = titulo;
+
+    // Creamos un nuevo elemento de descripción
+    const descripcionElemento = document.createElement('p');
+    descripcionElemento.textContent = descripcion;
+
+    // Añadimos los elementos al contenedor de la sección
+    const contenedor = document.querySelector('.seccion');
+    contenedor.appendChild(tituloElemento);
+    contenedor.appendChild(descripcionElemento);
+    })
+    .catch(error => console.error(error));
+}
+
+function enviarContenido(urltab){
+    const metaUrl = document.querySelector('meta[name="url"]').getAttribute('content');
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+    
+    fetch(metaUrl+urltab, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': csrfToken
+        },
+        body: JSON.stringify({
+            // datos a enviar en el cuerpo de la solicitud
+        })
+        })
+        .then(response => response.json())
+        .then(data => {
+        // procesar los datos de la respuesta
+        })
+        .catch(error => {
+        console.error('Ha ocurrido un error:', error);
+    });
 }
