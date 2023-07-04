4621e045296dd908b2ac39b35a798bf5a033e88f
Arturo <cosasscosas@gmail.com>
2023-07-04 17:51:29 +0200

    Quitado el fix temporal para la página de edición, comprobamos que es un número lo que viene ya que si no existe el id no va a mostrar nada. Se ha modificado que ya no exista resultTab y se crean los divs que necesiten las secciones. En javascript se quita que todo vaya a resultTab, quitamos la limpieza y ponemos cuando una pestaña ha sido cargada para reducir el número de llamadas que hacemos internamente para conseguir los datos.

--- app/views/admin/secciones/mainSections.php
+++ app/views/admin/secciones/mainSections.php
@@ -4,14 +4,17 @@
   <?php
   foreach($this->secciones as $section => $nombreSeccion):?>
     <li class="nav-item" role="presentation">
-      <a class="nav-link nav-link-tab" id="<?php echo $section; ?>-tab" href="#<?php echo $section; ?>" type="button" role="tab" aria-controls="<?php echo $section; ?>" aria-selected="true"><?php echo $nombreSeccion; ?></a>
+      <a class="nav-link nav-link-tab" id="<?php echo $section; ?>-tab" href="#<?php echo $section; ?>"  type="button" role="tab" aria-controls="<?php echo $section; ?>" aria-selected="true"><?php echo $nombreSeccion; ?></a>
     </li>
    
   <?php endforeach;?>
 </ul>
 <section class="col-md-12">
     <div class="tab-content" id="myTabContent">
-      <div class="tab-pane fade p-3" id="resultTab" role="tabpanel" aria-labelledby="b"></div>
+    <?php
+       foreach($this->secciones as $section => $nombreSeccion):?>
+        <div class="tab-pane fade p-3" id="<?php echo $section; ?>" role="tabpanel" aria-labelledby="<?php echo $section; ?>-tab"></div>
+      <?php endforeach;?>  
     </div>
 </section>
 <script>//alert(window.location.hash.substring(1)); //substring para quitar el #</script>
